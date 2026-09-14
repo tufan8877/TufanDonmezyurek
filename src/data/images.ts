2526fd987550162f271e53198431825ba7e1bd33
tufan8877 <tufandonmezyurek@gmail.com>
2026-09-14 18:56:58 +0200

    Replace homepage image with personal service photo

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,8 +1,6 @@
-// Zentrale Bilddaten – echte Fotografien von Pexels
-// Dieses Bild zeigt ein echtes Heizhaus / eine Heizungsanlage
-// Kann später durch ein eigenes Foto ersetzt werden
+// Zentrale Bilddaten
+// Hauptbild auf der Startseite: eigenes Foto aus dem technischen Arbeitsalltag
 
 export const images = {
-  // Hauptbild: echtes Heizhaus mit Heizungskessel, Rohrleitungen und Manometern
-  heizhaus: 'https://images.pexels.com/photos/7937300/pexels-photo-7937300.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  heizhaus: '/tufan-service.jpg',
 };
