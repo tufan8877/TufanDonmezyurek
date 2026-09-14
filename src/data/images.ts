0c2de4469df69aa41c257ebb30d0cf17ac4a5ffb
tufan8877 <tufandonmezyurek@gmail.com>
2026-09-14 19:01:31 +0200

    Fix Startseitenfoto und Cache aktualisieren

--- src/data/images.ts
+++ src/data/images.ts
@@ -2,5 +2,5 @@
 // Hauptbild auf der Startseite: eigenes Foto aus dem technischen Arbeitsalltag
 
 export const images = {
-  heizhaus: '/tufan-service.jpg',
+  heizhaus: '/tufan-service.jpg?v=2',
 };
